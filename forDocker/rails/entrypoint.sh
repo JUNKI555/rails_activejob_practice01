82eda073708df21ac8361e76247942d4ad07bc3e
JUNKI555 <junki555@gmail.com>
2021-02-25 18:18:07 +0900

    :wrench: init docker-compose

--- forDocker/rails/entrypoint.sh
+++ forDocker/rails/entrypoint.sh
@@ -1,7 +1,7 @@
-# Reference by https://matsuand.github.io/docs.docker.jp.onthefly/compose/rails/
 #!/bin/bash
 set -e
 
+# Reference by https://matsuand.github.io/docs.docker.jp.onthefly/compose/rails/
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
