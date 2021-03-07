b0486b879250f57d7293799352abd2df35cd07ca
JUNKI555 <junki555@gmail.com>
2021-03-07 13:51:16 +0900

    :sparkles: introduce job

--- app/jobs/sample_job.rb
+++ app/jobs/sample_job.rb
@@ -2,6 +2,21 @@ class SampleJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    logger.debug "Sample Job Start."
+
+    job_status = JobStatus.last
+
+    if job_status.blank?
+      logger.debug "no status error."
+      return
+    end
+
+    for i in 0..4
+      job_status.status += 1
+      job_status.save!
+      sleep(3)
+    end
+
+    logger.debug "Sample Job End."
   end
 end
