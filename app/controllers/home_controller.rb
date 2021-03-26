1b3162753ba178cba594db186e256aeb8ba2f58a
JUNKI555 <junki555@gmail.com>
2021-03-26 20:43:03 +0900

    :sparkles: add POST hash JSON

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,17 @@ class HomeController < ApplicationController
   end
 
   def get_post
+    # String Parameters
+    permit_parameters = params.permit(:key1, :key2, :key3, :name_input_text, keywords: [:keyword]).to_h
+
+    pp permit_parameters[:key1]
+    pp permit_parameters[:key2]
+    pp permit_parameters[:key3]
+    pp permit_parameters[:name_input_text]
+    permit_parameters[:keywords].each do |keyword|
+      pp keyword[:keyword]
+    end
+
     # binding.pry
-    pp params
-    pp 'get_post'
   end
 end
